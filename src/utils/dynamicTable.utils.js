b298fa5686199c4dba289d9167b2a4231ba45f7f
CodenCode-Naman <nbhatia_be19@thapar.edu>
2023-03-10 09:50:23 +0530

    feat: add dynamic table

---
+++ src/utils/dynamicTable.utils.js
@@ -0,0 +1,32 @@
+const db = require('../../db/models');
+
+const dynamicTable = (tableName, fields) => {
+  const tableFields = {};
+  fields.forEach((field) => {
+    tableFields[field] = {
+      type: db.Sequelize.STRING,
+    };
+  });
+  const tableAttributes = {
+    id: {
+      type: db.Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    ...tableFields,
+  };
+  db.sequelize.define(tableName, tableAttributes, {
+    freezeTableName: true,
+  });
+  db.sequelize.sync()
+      .then(() => {
+        console.log('Database and table synced');
+      })
+      .catch((err) => {
+        console.log('Error in database', err);
+      });
+};
+
+module.exports = {
+  dynamicTable,
+};
